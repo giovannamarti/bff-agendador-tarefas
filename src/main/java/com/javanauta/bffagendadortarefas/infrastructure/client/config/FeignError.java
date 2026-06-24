a790a068275e204079d600d91a7b2a280d857480
Giovanna <giovannamartinin@MacBook-Air-de-Giovanna.local>
2026-06-24 11:04:10 -0300

    adicionado endpoint para busca de cep

--- src/main/java/com/javanauta/bffagendadortarefas/infrastructure/client/config/FeignError.java
+++ src/main/java/com/javanauta/bffagendadortarefas/infrastructure/client/config/FeignError.java
@@ -7,9 +7,16 @@ import com.javanauta.bffagendadortarefas.infrastructure.exception.UnauthorizedEx
 import feign.Response;
 import feign.codec.ErrorDecoder;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
 public class FeignError implements ErrorDecoder {
     @Override
     public Exception decode(String s, Response response) {
+
+        String mensagemErro = mensagemErro(response);
+
         switch (response.status()) {
             case 409:
                 return new ConflictException("Erro atributo ja existente");
@@ -17,10 +24,27 @@ public class FeignError implements ErrorDecoder {
                 return new ResourceNotFoundException("Erro atributo não encontrado");
             case 401:
                 return new UnauthorizedException("Erro usuário não autorizado");
+            case 400:
+                return new IllegalArgumentException("Erro ao buscar CEP");
             default:
                 return new BusinessException("Erro de servidor");
-        }
 
+        }
     }
 
+    private String mensagemErro(Response response) {
+        try {
+            if (Objects.isNull(response.body())) {
+                return "";
+            }
+            return new String(response.body().asInputStream().readAllBytes(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
+
+
+
+
